fc57503eb60bcf445444ba3ae5811a4f8f8338fe
Carlos Ramirez <carlosramirez686@gmail.com>
2022-01-03 13:56:33 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun (task = "roller-skate") {
+    return `This Saturday, I want to ${task}!`;
+}
+function mondayWork (task = "go to the office") {
+    return `This Monday, I will ${task}.`
+}
+function wrapAdjective (style ="*") {
+    return function (name = 'special') {
+        return `You are ${style}${name}${style}!`;
+    }
+}
